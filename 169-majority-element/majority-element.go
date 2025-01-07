7126c8bd7b4939becd73f50ac6571ebdccbacd03
marco-zulian <162844424+marco-zulian@users.noreply.github.com>
2025-01-07 09:11:10 -0300

    Time: 1 ms (32.59%) | Memory: 8.5 MB (65.47%) - LeetSync

--- 169-majority-element/majority-element.go
+++ 169-majority-element/majority-element.go
@@ -1,6 +1,6 @@
 func majorityElement(nums []int) int {
     counts := map[int]int{}
-    majority := int(math.Floor(float64(len(nums)) / 2))
+    majority := len(nums) / 2
 
     for _, num := range nums {
         counts[num]++
