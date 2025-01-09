28a74aee875f346829af8293ad4bcd918de387bf
marco-zulian <162844424+marco-zulian@users.noreply.github.com>
2025-01-09 02:38:13 -0300

    Time: 0 ms (100.00%) | Memory: 6.8 MB (83.33%) - LeetSync

--- 280-wiggle-sort/wiggle-sort.go
+++ 280-wiggle-sort/wiggle-sort.go
@@ -1,17 +1,13 @@
 func wiggleSort(nums []int)  {
-    result := make([]int, len(nums))
-    for i,v := range nums {
-        result[i] = v
-    }
-    slices.Sort(result)  
-
-    i, j, k := 0, len(nums)-1, 0
-    for k < len(nums) {
-        nums[k] = result[i]
-        i++
-        k++
-        if k < len(nums) { nums[k] = result[j] }
-        j--
-        k++
+    for i,_ := range nums[1:] {
+        if i % 2 == 0 {
+            if nums[i] > nums[i+1] {
+                nums[i], nums[i+1] = nums[i+1], nums[i]
+            }
+        } else {
+            if nums[i] < nums[i+1] {
+                nums[i], nums[i+1] = nums[i+1], nums[i]
+            }
+        }
     }
 }
