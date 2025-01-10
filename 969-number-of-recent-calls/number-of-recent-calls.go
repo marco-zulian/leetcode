3262c402cac7687aa68a384c6c4549f9d146c2ec
marco-zulian <162844424+marco-zulian@users.noreply.github.com>
2025-01-10 16:14:29 -0300

    Time: 49 ms (23.42%) | Memory: 9.6 MB (67.13%) - LeetSync

---
+++ 969-number-of-recent-calls/number-of-recent-calls.go
@@ -0,0 +1,29 @@
+type RecentCounter struct {
+    times []int
+}
+
+
+func Constructor() RecentCounter {
+    return RecentCounter{}
+}
+
+
+func (this *RecentCounter) Ping(t int) int {
+    removed := 0
+
+    for _, v := range this.times {
+        if v < t - 3000 {
+            removed++
+        }
+    }
+
+    this.times = append(this.times[removed:], t)
+    return len(this.times)
+}
+
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Ping(t);
+ */
