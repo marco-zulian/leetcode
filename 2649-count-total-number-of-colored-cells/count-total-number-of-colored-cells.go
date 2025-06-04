11cb4ab69f1590212770b7cde5aa6ba6b2cd62b4
marco-zulian <162844424+marco-zulian@users.noreply.github.com>
2025-06-03 21:25:04 -0300

    Time: 0 ms (100.00%) | Memory: 3.9 MB (22.73%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.go
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.go
@@ -1,13 +1,3 @@
 func coloredCells(n int) int64 {
-    if n == 1 {
-        return 1
-    }
-
-    if n == 2 {
-        return 5
-    }
-
-    cellsPaintedOnLastExpansion := 4 * (n - 1)
-    cumulativeCellsPainted := int64((5 + cellsPaintedOnLastExpansion) * (n - 1) / 2)
-    return cumulativeCellsPainted - int64((n - 3) / 2)
+    return 1 + 4 * int64(n) * int64(n-1) / 2
 }
