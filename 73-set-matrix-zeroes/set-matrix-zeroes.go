5de6885052285c6092c096afcb7f48d8c283293b
marco-zulian <162844424+marco-zulian@users.noreply.github.com>
2025-08-19 07:23:38 -0300

    Time: 0 ms (100.00%) | Memory: 7.6 MB (92.67%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.go
+++ 73-set-matrix-zeroes/set-matrix-zeroes.go
@@ -1,26 +1,47 @@
 func setZeroes(matrix [][]int)  {
-    swapped := make([][]bool, len(matrix))
-    for i := range len(matrix) {
-        swapped[i] = make([]bool, len(matrix[0]))
+
+    firstRow, firstCol := false, false
+
+    for i := 0; i < len(matrix); i++ {
+        if matrix[i][0] == 0 {
+            firstCol = true
+            break
+        }
     }
-    
-    for i, row := range matrix {
-        for j, cell := range row {
-            if cell == 0 && !swapped[i][j] {
-                for k := range row {
-                    if matrix[i][k] != 0 {
-                        matrix[i][k] = 0
-                        swapped[i][k] = true   
-                    }
-                }
-                
-                for k := range matrix {
-                    if matrix[k][j] != 0 {
-                        matrix[k][j] = 0
-                        swapped[k][j] = true
-                    }
-                }
+
+    for i := 0; i < len(matrix[0]); i++ {
+        if matrix[0][i] == 0 {
+            firstRow = true
+            break
+        }
+    }
+
+    for i := 1; i < len(matrix); i++ {
+        for j := 1; j < len(matrix[i]); j++ {
+            if matrix[i][j] == 0 {
+                matrix[i][0] = 0
+                matrix[0][j] = 0
             }
+        }
+    }
+
+    for i := 1; i < len(matrix); i++ {
+        for j := 1; j < len(matrix[i]); j++ {
+            if matrix[i][0] == 0 || matrix[0][j] == 0 {
+                matrix[i][j] = 0
+            }
+        }
+    }
+
+    if firstRow {
+        for i := 0; i < len(matrix[0]); i++ {
+            matrix[0][i] = 0
+        }
+    }
+
+    if firstCol {
+        for i := 0; i < len(matrix); i++ {
+            matrix[i][0] = 0
         }
     }
 }
