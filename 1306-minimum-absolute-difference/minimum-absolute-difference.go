160234f70b3b79a8c2967e0143cd07bc7755d778
marco-zulian <162844424+marco-zulian@users.noreply.github.com>
2025-08-25 15:56:18 -0300

    Time: 16 ms (39.24%) | Memory: 45 MB (5.06%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.go
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.go
@@ -1,16 +1,32 @@
 func minimumAbsDifference(arr []int) [][]int {
     result := [][]int{}
-    sort.Ints(arr)
-    
-    minDiff := 2000001
-    for i := range arr[:len(arr)-1] {
-        if arr[i+1] - arr[i] < minDiff {
-            minDiff = arr[i+1] - arr[i]
-            result = [][]int{[]int{arr[i], arr[i+1]}}
-        } else if arr[i+1] - arr[i] == minDiff {
-            result = append(result, []int{arr[i], arr[i+1]})
-        }
+    min, max := 1000001, -1000001
+
+    for _, n := range arr {
+        if n > max { max = n }
+        if n < min { min = n }
     }
-    
+
+    buckets := make([]int, max - min + 1)
+    for _, n := range arr {
+        buckets[n-min]++
+    }
+
+    prev := -50000000
+    minDiff := 2000001
+    for i, v := range buckets {
+        if v == 0 { continue }
+        distance := i + min - prev
+
+        if distance < minDiff {
+            minDiff = distance
+            result = [][]int{[]int{prev, min+i}}
+        } else if distance == minDiff {
+            result = append(result, []int{prev, min+i})
+        }
+
+        prev = i + min
+    }
+
     return result
 }
